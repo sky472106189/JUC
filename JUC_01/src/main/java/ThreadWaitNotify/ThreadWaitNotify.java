653ea879cded6a27cfd7f62d41c203a217235922
ywwup <ywwup@isoftstone.com>
2020-10-12 17:07:03 +0800

    Lambda学习

--- JUC_01/src/main/java/ThreadWaitNotify/ThreadWaitNotify.java
+++ JUC_01/src/main/java/ThreadWaitNotify/ThreadWaitNotify.java
@@ -7,31 +7,57 @@ package ThreadWaitNotify;
  *
  * 1.   高内聚低耦合前提下，线程操作资源类
  * 2.   判断/干活/通知
+ * 3.   多线程交互中，必须要防止多线程中的虚假唤醒，也即(判断使用While，不能用if)
+ *              因为if是判断，while是循环,虚假唤醒是因为没有循环的原因
  */
 public class ThreadWaitNotify {
 
     public static void main(String[] args) throws Exception{
         Number number = new Number();
-        //增加
+        //生产
         new Thread(()->{
             for (int i = 0; i < 10; i++) {
                 try {
+                    Thread.sleep(100);
                     number.add();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
         },"A").start();
-        //减少
+        //消费
         new Thread(()->{
             for (int i = 0; i < 10; i++) {
                 try {
+                    Thread.sleep(200);
                     number.descr();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
         },"B").start();
+        //生产
+        new Thread(()->{
+            for (int i = 0; i < 10; i++) {
+                try {
+                    Thread.sleep(300);
+                    number.add();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        },"C").start();
+        //消费
+        new Thread(()->{
+            for (int i = 0; i < 10; i++) {
+                try {
+                    Thread.sleep(400);
+                    number.descr();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        },"D").start();
     }
 
 }
@@ -39,7 +65,7 @@ class Number{//资源类
     private int number = 0;
 
     public synchronized void add() throws InterruptedException {
-        if(number!=0){
+        while(number!=0){
             this.wait();
         }
         //干活
@@ -50,7 +76,7 @@ class Number{//资源类
     };
 
     public synchronized void descr() throws InterruptedException {
-        if(number==0){
+        while(number==0){
             this.wait();
         }
         //干活
